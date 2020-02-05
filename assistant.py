a893abd12a2aa895f8c132b09fcea8af866066e6
fwe <florian.fynn.weber@gmail.com>
2020-02-05 10:55:48 +0100

    update

--- assistant.py
+++ assistant.py
@@ -2,23 +2,26 @@ import pyperclip
 import time
 import traceback
 import json
+import clipboard
 
-
-questions = json.load(open("questions/" + input("Question file (e.g. '2_1'): ") + ".json"))
-
+questions = json.load(open("questions/" +input("Question file (e.g. '2_1'): ") + ".json"))
+old = ""
 while True:
     try:
+
         question = pyperclip.paste()
-        if question and not question.startswith("A: "):
+        if question and not question.startswith("A: ") and question !=old:
             answers = []
             for q, a in questions:
                 if question.strip() in q:
                     answers.append(a)
-
-            pyperclip.copy("A: " + " //// ".join(["; ".join(a) for a in answers]))
+            old = question
+            print("A: " + " //// ".join(["; ".join(a) for a in answers]))
+            #clipboard.copy("A: " + " //// ".join(["; ".join(a) for a in answers]))
 
     except:
-        traceback.print_exc()
+        # traceback.print_exc()
+        pass
 
     finally:
         time.sleep(0.5)
